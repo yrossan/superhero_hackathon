7b7a33471f733dc3e2a1d0f1b4bf271f42951fa6
Yas Rossan <yaaseen.rossan95@gmail.com>
2023-05-25 15:45:17 +0100

    fixed heroGallery & made README

--- src/components/HeroImage/index.jsx
+++ src/components/HeroImage/index.jsx
@@ -1,10 +1,13 @@
 import React from 'react'
+import '../../App.css'
 
 const HeroImage = ({hero}) => {
   return (
     <div className='gallery-image'>
         <img src = {hero.image.url}/>
     </div>
+
+    
   )
 }
 
